9bbbefd07cc5ba1f0c2bc79cd5051e06029328a1
Delphi89 <44812157+Delphi89@users.noreply.github.com>
2019-07-18 00:52:36 +0300

    Add files via upload

---
+++ Utillities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Utillities:
+
+    def listing(optimizer, SCI_SGD_MOMENTUM, SCI_BN_MOMENTUM, SCI_L_SECOND, SCI_LR, SCI_RELU, SCI_BIAS, SCI_loss_type, REGULARIZATION, SCI_BATCH_SIZE, SCI_DROPOUT):
+        print('Optimization: ', optimizer)
+        if optimizer == 'SGD':
+            print('MM: ',SCI_SGD_MOMENTUM)
+        print('Batch Normalization Momentum: ',SCI_BN_MOMENTUM)   
+        print('Nodes: ', SCI_L_SECOND)         
+        print('LR: ', SCI_LR)         
+        print('RELU: ', SCI_RELU)       
+        print('BIAS: ', SCI_BIAS)   
+        print('Loss Type: ', SCI_loss_type)   
+        print('REGULARIZATION: ', REGULARIZATION)    
+        print('BATCH_SIZE: ', SCI_BATCH_SIZE)
+        print('Dropout: ', SCI_DROPOUT)
+
