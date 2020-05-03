dad335766da9a0982d477268f1dd6bda6107d313
pauld <paul.diaconescu@gmail.com>
2020-05-03 18:34:41 +0300

    Credit Score: 107

---
+++ German-Credit-Private--master/Utillities9.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import torch.optim as optim
+from adamw import AdamW
+
+
+class Utillities:
+    
+    def variable_learning_rate(existing_learning_rate, Min, Max, change_rate, flag):
+        if flag:
+            new_learning_rate = existing_learning_rate * (change_rate * 1.1)
+            if new_learning_rate > Max:
+                flag = False
+        else:
+            new_learning_rate = existing_learning_rate / (change_rate *1.05)
+            if new_learning_rate < Min :
+                flag = True
+        #print(existing_learning_rate, new_learning_rate, flag, Min, Max, change_rate)
+        return new_learning_rate, flag
+
+
+    def listing(optimizer, SCI_SGD_MOMENTUM, SCI_BN_MOMENTUM, SCI_L_SECOND, SCI_LR, SCI_RELU, SCI_BIAS, SCI_loss_type, REGULARIZATION, SCI_BATCH_SIZE, SCI_DROPOUT, SCI_LAST_LAYER, SCI_ACT_LAYER):
+        print('Optimization: ', optimizer)
+        if optimizer == 'SGD':
+            print('MM: ',SCI_SGD_MOMENTUM)
+        print('Batch Normalization Momentum: ',SCI_BN_MOMENTUM)   
+        print('Nodes: ', SCI_L_SECOND)         
+        print('LR: ', SCI_LR)         
+        print('RELU: ', SCI_RELU)       
+        print('BIAS: ', SCI_BIAS)   
+        print('Loss Type: ', SCI_loss_type)   
+        print('REGULARIZATION: ', REGULARIZATION)    
+        print('BATCH_SIZE: ', SCI_BATCH_SIZE)
+        print('Dropout: ', SCI_DROPOUT)
+        print('LAST LAYER ', SCI_LAST_LAYER)
+        print('Activation LAYER ', SCI_ACT_LAYER)
+ 
+
+    def optimization_algorithms(SCI_optimizer,cnn, LR, SCI_SGD_MOMENTUM, REGULARIZATION):  
+
+        if type(SCI_optimizer) is str:
+            if (SCI_optimizer == 'Adam') :
+                optimizer = optim.Adam(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay=REGULARIZATION)
+            if (SCI_optimizer == 'AMSGrad') :
+                optimizer = optim.Adam(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay=REGULARIZATION, amsgrad=True)
+            if (SCI_optimizer == 'AdamW')  :
+                optimizer = AdamW(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay = REGULARIZATION)   
+            if (SCI_optimizer == 'RMSprop') :
+                optimizer = optim.RMSprop(cnn.parameters(), lr=LR)              
+            #if (SCI_optimizer == 'SparseAdam') or (int(SCI_optimizer) == 4) :
+                #optimizer = optim.SparseAdam(cnn.parameters(), lr=LR)            
+            if (SCI_optimizer == 'SGD')  :
+                optimizer = optim.SGD(cnn.parameters(), lr=LR, momentum=SCI_SGD_MOMENTUM, weight_decay=REGULARIZATION)
+            if (SCI_optimizer == 'Adadelta')  :
+                optimizer = optim.Adadelta(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)
+            if (SCI_optimizer == 'Rprop')  :
+                optimizer = optim.Rprop(cnn.parameters(), lr=LR)             
+            #if (SCI_optimizer == 'Adagrad') or (int(SCI_optimizer) == 7) :
+            #    optimizer = optim.Adagrad(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)
+            if (SCI_optimizer == 'Adamax')  :
+                optimizer = optim.Adamax(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)   
+            if (SCI_optimizer == 'ASGD') :
+                optimizer = optim.ASGD(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)               
+            #if (SCI_optimizer == 'LBFGS') or (int(SCI_optimizer) == 10) :
+                #optimizer = optim.LBFGS(cnn.parameters(), lr=LR)   
+        else: 
+            if (int(SCI_optimizer) == 5) :
+                optimizer = optim.Adam(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay=REGULARIZATION)
+            if (int(SCI_optimizer) == 3) :
+                optimizer = optim.Adam(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay=REGULARIZATION, amsgrad=True)
+            if (int(SCI_optimizer) == 7) :
+                optimizer = AdamW(cnn.parameters(), lr=LR, betas=(0.01, 0.999), weight_decay = REGULARIZATION)   
+            if (int(SCI_optimizer) == 4) :
+                optimizer = optim.RMSprop(cnn.parameters(), lr=LR)              
+            #if (SCI_optimizer == 'SparseAdam') or (int(SCI_optimizer) == 4) :
+                #optimizer = optim.SparseAdam(cnn.parameters(), lr=LR)            
+            if (int(SCI_optimizer) == 2) :
+                optimizer = optim.SGD(cnn.parameters(), lr=LR, momentum=SCI_SGD_MOMENTUM, weight_decay=REGULARIZATION)
+            if (int(SCI_optimizer) == 6) :
+                optimizer = optim.Adadelta(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)
+            if (int(SCI_optimizer) == 1) :
+                optimizer = optim.Rprop(cnn.parameters(), lr=LR)             
+            #if (SCI_optimizer == 'Adagrad') or (int(SCI_optimizer) == 7) :
+            #    optimizer = optim.Adagrad(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)
+            if (int(SCI_optimizer) == 8) :
+                optimizer = optim.Adamax(cnn.parameters(), lr=LR, weight_decay=REGULARIZATION)   
+            if (int(SCI_optimizer) == 9) :
+                optimizer = optim.ASGD(cnn.parameters(), lr=LR, lambd=0.0001, alpha=0.75, t0=1000000.0, weight_decay=REGULARIZATION)       
+         
+            #if (SCI_optimizer == 'LBFGS') or (int(SCI_optimizer) == 10) :
+                #optimizer = optim.LBFGS(cnn.parameters(), lr=LR)        
+      
+        return optimizer
+    
+    
+        
---
+++ German-Credit-Private--master/ex281.py
@@ -0,0 +1,436 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# best score: 107
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as Data
+import torch.optim as optim
+import torchvision
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils import data
+import numpy as np
+import os
+import cProfile, pstats, io
+from pstats import SortKey
+from random import randint  
+import matplotlib.pyplot as mp
+#import matplotlib.pyplot as plt
+
+from Utillities7 import Utillities
+from cnn_model12 import CNN6
+from early_stopping4 import EarlyStopping
+from dataset5 import dataset  
+
+
+print("PyTorch Version: ",torch.__version__)
+print("Torchvision Version: ",torchvision.__version__)
+
+
+
+if os.path.exists("checkpoint.pt"):
+    os.remove("checkpoint.pt")
+
+torch.manual_seed(67578)   # reproducible
+
+OPTIMIZATION_PLUGIN = 'Bayesian' # 'Bayesian' or 'Scikit' or 'GradDescent'
+#Bayesian requires: $ conda install -c conda-forge bayesian-optimization
+
+GET_STATS = False
+GPU_SELECT = 2 # can be 0, 1, 2 (both)
+PARALLEL_PROCESSES = 2
+TRIALS = 3000
+RANDOM_STARTS = 500
+LR  = 1e-5                    # learning rate
+SCI_LR =  1e-5
+LR2 = 1e-5
+SCI_MM = 0.5                  # momentum - used only with SGD optimizer
+MM = 0.5
+L_FIRST = 24                  # initial number of channels
+KERNEL_X = 24
+patience = 1000                # if validation loss not going down, wait "patience" number of epochs
+accuracy = 10
+MaxCredit = -800
+
+LR_MIN = 0.00005
+LR_MAX = 0.3
+
+CreditVector = np.zeros(RANDOM_STARTS + TRIALS)
+CreditVector = CreditVector - 800
+CreditVec = np.zeros(RANDOM_STARTS + TRIALS)
+count = 0
+
+pr = cProfile.Profile()
+
+if GET_STATS:
+    pr.enable()
+    
+
+if GPU_SELECT == 2:
+    if torch.cuda.device_count() > 1:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("Using", torch.cuda.device_count(), "NVIDIA 1080TI GPUs!")
+
+if GPU_SELECT == 1:
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")    
+    print("Using one (the second) NVIDIA 1080TI GPU!")
+
+if GPU_SELECT == 0:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")       
+    print("Using one (the first) NVIDIA 1080TI GPU!")
+
+
+# In[2]:
+
+
+
+early_stopping = EarlyStopping(patience=patience, verbose=True)  # initialize the early_stopping object
+
+# Counter for the execution time
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
+
+start.record()
+
+
+
+
+
+if OPTIMIZATION_PLUGIN == 'Bayesian' :
+    from bayes_opt import BayesianOptimization
+    #print("BayesianOptimization Version: ",bayes_opt.__version__)
+    
+    #def black_box_function(x, y):
+    def objective(SCI_RELU, SCI_BIAS, SCI_loss_type,
+                  SCI_optimizer, SCI_LR, SCI_MM, 
+                  SCI_REGULARIZATION, SCI_EPOCHS, SCI_BATCH_SIZE, 
+                  SCI_DROPOUT, SCI_L_SECOND, SCI_BN_MOMENTUM, SCI_SGD_MOMENTUM,
+                  SCI_BN_EPS, SCI_BN_STATS, SCI_LAST_LAYER, SCI_ACT_LAYER):
+        global count, PercentVector, PercentVec, device, MaxCredit
+
+        
+        SCI_BATCH_SIZE = int(SCI_BATCH_SIZE)   
+        SCI_LAST_LAYER = int(SCI_LAST_LAYER)       
+        SCI_ACT_LAYER =int(SCI_ACT_LAYER)
+        SCI_MM = round(SCI_MM,3)                                # real with three decimals between (0.001, 0.999)
+        SCI_LR = round(SCI_LR,5)                                # real with five decimals between(1e-4, 7e-1)            
+        SCI_DROPOUT = round(SCI_DROPOUT,2)                      # real with two decimals between (0, 0.4)
+        SCI_L_SECOND = int(SCI_L_SECOND)                        # integer between 2 and 64
+        SCI_EPOCHS = int(SCI_EPOCHS)                            # integer between (100, 500)
+        SCI_BN_MOMENTUM = round(SCI_BN_MOMENTUM,2)              # real with two decimals between (0, 0.99)
+        SCI_SGD_MOMENTUM = round(SCI_SGD_MOMENTUM,2)            # real with two decimals between (0, 0.99) 
+        SCI_loss_type = int(SCI_loss_type)                      # integer between 1 and 3 ('CrossEntropyLoss', 'MultiMarginLoss','NLLLoss')
+        SCI_BN_EPS = int(SCI_BN_EPS)
+
+            
+        if int(SCI_RELU) == 1 :                                 # integer between 1 and 2 ('True', 'False')
+            SCI_RELU = True      
+        else:
+            SCI_RELU = False      
+        if int(SCI_BIAS) == 1 :                                 # integer between 1 and 2 ('True', 'False')
+            SCI_BIAS = True      
+        else:
+            SCI_BIAS = False  
+ 
+        SCI_REGULARIZATION = float(str(SCI_REGULARIZATION))
+    
+    
+  
+        if SCI_BN_EPS == 0:
+            BN_EPS = 5e-6
+        if SCI_BN_EPS == 1:
+            BN_EPS = 1e-5
+        if SCI_BN_EPS == 2:
+            BN_EPS = 5e-6
+        if SCI_BN_EPS == 3:
+            BN_EPS = 1e-6             
+        if SCI_BN_EPS == 4:
+            BN_EPS = 5e-7     
+        if SCI_BN_EPS == 5:
+            BN_EPS = 1e-7
+        if SCI_BN_EPS == 6:
+            BN_EPS = 5e-8
+        if SCI_BN_EPS == 7:
+            BN_EPS = 1e-8
+        if SCI_BN_EPS == 8:
+            BN_EPS = 3e-7             
+        if SCI_BN_EPS == 9:
+            BN_EPS = 8e-7    
+        if SCI_BN_EPS == 10:
+            BN_EPS = 1e-4             
+        if SCI_BN_EPS == 11:
+            BN_EPS = 5e-4    
+        if SCI_BN_EPS == 12:
+            BN_EPS = 8e-6    
+        if SCI_BN_EPS == 13:
+            BN_EPS = 1e-6             
+        if SCI_BN_EPS == 14:
+            BN_EPS = 8e-5                                           
+        print('BN Batch EPS: ', BN_EPS)    
+        
+        SCI_BN_STATS = int(SCI_BN_STATS)
+        if SCI_BN_STATS == 0:
+            BN_STATS = True
+        if SCI_BN_STATS == 1:
+            BN_STATS = False
+        print('BN Batch STATS: ', BN_STATS)     
+            
+        
+        
+        cnn = CNN6(L_FIRST, SCI_L_SECOND, KERNEL_X,
+                   SCI_BIAS, SCI_BN_MOMENTUM, SCI_RELU,
+                   SCI_DROPOUT, dataset.CLASSES,
+                   BN_EPS, BN_STATS, SCI_LAST_LAYER, SCI_ACT_LAYER)     
+
+        optimizer = Utillities.optimization_algorithms(SCI_optimizer,cnn, SCI_LR, SCI_SGD_MOMENTUM,
+                                                       SCI_REGULARIZATION)
+        
+        if GPU_SELECT == 2:
+            if torch.cuda.device_count() > 1:
+                cnn = nn.DataParallel(cnn,device_ids=[0, 1], dim = 0) 
+            cnn = cnn.cuda()                
+        if GPU_SELECT == 1:
+            cnn.to(device)  
+        if GPU_SELECT == 0:
+            cnn.to(device)        
+
+          
+        cnn.apply(CNN6.weights_init2)    
+        #cnn.apply(CNN6.weights_reset)        
+        cnn.share_memory()
+     
+        loss_func = nn.CrossEntropyLoss()
+
+        def create_loss(LOSS):   
+            print('*** LOSS ******:',  LOSS)
+            if LOSS == 1:
+                loss_func = nn.BCELoss()
+                print('*********  BCELoss')
+            if LOSS == 2:
+                loss_func = nn.MultiMarginLoss()
+                print('*********  MMLoss')                               
+            if LOSS == 4:
+                loss_func = nn.CrossEntropyLoss() 
+                print('********* CrossEntropyLoss ')
+            if LOSS == 3:
+                loss_func = nn.TripletMarginLoss() 
+                print('********* TripletMarginLoss ')                  
+                                 
+            return loss_func
+               
+        MM = float(str(SCI_MM))
+
+        LR = float(str(SCI_LR))
+        train_losses = []         # to track the training loss as the model trains
+        output = 0
+        loss = 0
+        accuracy = 0
+        early_stopping.counter = 0
+        early_stopping.best_score = None
+        early_stopping.early_stop = False
+        early_stopping.verbose = False  
+        TEST_RESULTS = torch.zeros(1,2)
+    
+        loss_type = create_loss(SCI_loss_type)
+        
+        #cnn, optimizer = amp.initialize(
+        #   cnn, optimizer, opt_level=BITS, 
+        #   keep_batchnorm_fp32=True, loss_scale="dynamic"
+        #)
+    
+        Utillities.listing(optimizer, SCI_SGD_MOMENTUM, SCI_BN_MOMENTUM, 
+                           SCI_L_SECOND, SCI_LR, SCI_RELU, 
+                           SCI_BIAS, SCI_loss_type, SCI_REGULARIZATION, 
+                           SCI_BATCH_SIZE, SCI_DROPOUT, SCI_LAST_LAYER, SCI_ACT_LAYER)
+
+    
+        # Data Loader for easy mini-batch return in training
+        SCI_BATCH_SIZE = int(SCI_BATCH_SIZE)
+        train_loader = Data.DataLoader(dataset = dataset.train_dataset, batch_size = SCI_BATCH_SIZE, shuffle = True, num_workers = 0, drop_last=True, pin_memory=True)
+        validation_loader = Data.DataLoader(dataset = dataset.validation_dataset, batch_size = 123, shuffle = True, num_workers = 0, drop_last=True, pin_memory=True)    
+        test_loader = Data.DataLoader(dataset = dataset.test_dataset, batch_size = 300, shuffle = True, num_workers = 0, drop_last=True, pin_memory=True)
+    
+        flag = True;
+    
+        for epoch in range(SCI_EPOCHS):
+            loss = None        
+            cnn.train().cuda()
+            for step, (train_data, train_target) in enumerate(train_loader):   
+                train_data, train_target = train_data.to(device), train_target.to(device)
+                output = cnn(train_data)                # forward pass: compute predicted outputs by passing inputs to the model     
+                loss = loss_func(output, train_target)
+                train_losses.append(loss.item())
+                #batch_loss.backward()
+                loss.backward()                               # backward pass: compute gradient of the loss with respect to model parameters
+                optimizer.zero_grad()
+                optimizer.step()                              # perform a single optimization step (parameter update)
+      
+            cnn.eval().cuda()                 # switch to evaluation (no change) mode           
+            valid_loss = 0
+            accuracy = 0
+            running_loss = 0.0
+            with torch.no_grad():
+                for step, (validation_data, validation_target) in enumerate(validation_loader):
+                    validation_data, validation_target = validation_data.to(device), validation_target.to(device)
+                    output = cnn(validation_data)      
+                    valid_loss = loss_func(output, validation_target)
+                    running_loss += valid_loss.item()
+                epoch_val_loss = running_loss / len(validation_loader)   
+                   
+                if epoch % 2 == 0: 
+                    SCI_LR, flag = Utillities.variable_learning_rate(SCI_LR, LR_MIN, LR_MAX, 1.5, flag)
+                    optimizer = Utillities.optimization_algorithms(SCI_optimizer,cnn, SCI_LR, SCI_SGD_MOMENTUM,
+                                                       SCI_REGULARIZATION)
+                    SCI_DROPOUT = SCI_DROPOUT / 1.02
+                    cnn = CNN6(L_FIRST, SCI_L_SECOND, KERNEL_X,
+                               SCI_BIAS, SCI_BN_MOMENTUM, SCI_RELU,
+                               SCI_DROPOUT, dataset.CLASSES,
+                               BN_EPS, BN_STATS, SCI_LAST_LAYER, SCI_ACT_LAYER) 
+                    cnn = nn.DataParallel(cnn,device_ids=[0, 1], dim = 0) 
+                    cnn = cnn.cuda()                 
+            early_stopping(epoch_val_loss, cnn)        #print('validation loss:',epoch_val_loss)                
+            train_losses = []
+                    
+            if early_stopping.early_stop:
+                if os.path.exists('checkpoint.pt'):
+                    #cnn = TheModelClass(*args, **kwargs)
+                    print("Loaded the model with the lowest Validation Loss!")
+                    cnn.load_state_dict(torch.load('checkpoint.pt'))  # Choose whatever GPU device number you want
+                    cnn.to(device)
+                break
+            running_loss = 0.0
+        cnn.eval()
+        class_correct = list(0. for i in range(1000))
+        class_total = list(0. for i in range(1000))
+        with torch.no_grad():
+            for (test_data, test_target) in test_loader:
+                test_data, test_target = test_data.to(device), test_target.to(device)
+                outputs = cnn(test_data)
+                _, predicted = torch.max(outputs, 1)
+                c = (predicted == test_target).squeeze()
+                dx = ((c.cpu()).numpy()).astype(int)
+                #dx = 600
+                for i in range(test_target.size(0)):
+                    label = test_target[i]
+                    class_correct[label] += c[i].item()
+                    class_total[label] += 1
+
+        for i in range(dataset.CLASSES):
+            TEST_RESULTS[0,i] = class_correct[i] / dataset.TESTED_ELEMENTS[i]
+            print('Class: ',i,' accuracy: ', TEST_RESULTS[0,i])   
+            print('Class: ',i,' correct: ', class_correct[i],' of ',dataset.TESTED_ELEMENTS[i])
+
+        #mp.matshow(dx.reshape((20, 30)))
+        #mp.ylabel('Correct Results')
+        #mp.colorbar()
+        #mp.show()
+        percent = (TEST_RESULTS[0,0]+TEST_RESULTS[0,1])/2
+        print('Final percentage: ',percent)
+        CreditCost = 0
+        CreditCost = int((1 - TEST_RESULTS[0,0]) * dataset.TESTED_ELEMENTS[0] + (1 - TEST_RESULTS[0,1]) * dataset.TESTED_ELEMENTS[1] * 5)
+    
+        if TEST_RESULTS[0,0] < 0.05 or TEST_RESULTS[0,1] < 0.05 :
+            CreditCost = CreditCost + 300
+    
+        print('Last epoch: ', epoch)
+    
+        if os.path.exists('checkpoint.pt'):  
+            os.remove('checkpoint.pt') 
+
+
+        #print('Credit Cost: ',CreditCost)
+        #CreditCost = CreditCost + (SCI_SGD_MOMENTUM + SCI_DROPOUT + SCI_BATCH_SIZE + SCI_L_SECOND + SCI_optimizer + SCI_loss_type+ SCI_LR+ SCI_BN_EPS+SCI_BN_STATS+SCI_LAST_LAYER+SCI_ACT_LAYER)/10000
+        print('Credit Cost: ',CreditCost)
+        
+        if -CreditCost > MaxCredit : 
+            MaxCredit = -CreditCost
+        print('Best Score So Far: ',MaxCredit)   
+        print()
+        print()
+        
+        CreditVector[count] = MaxCredit    
+        CreditVec[count] = count
+        # plot the data
+        #fig = mp.figure()
+        #ax = fig.add_subplot(1, 1, 1)
+        #ax.plot(CreditVec, -CreditVector, color='tab:orange')
+        #print(CreditVec, -CreditVector)
+        count = count + 1
+        # display the plot
+        #mp.show()       
+        return CreditCost
+    
+    
+    optimizer = BayesianOptimization(
+        f=objective,
+        #pbounds=pbounds,
+        pbounds={'SCI_RELU': (1,1.99), 
+                 'SCI_BIAS': (1,1.99), 
+                 'SCI_loss_type': (2, 2.99), 
+                 'SCI_optimizer': (4, 4.99),
+                 'SCI_LR': (0.23679, 0.23679), 
+                 'SCI_MM': (0.001, 0.999), 
+                 'SCI_REGULARIZATION': (0.06542977515465478, 0.06542977515465478), 
+                 'SCI_EPOCHS': (2000, 2000), 
+                 'SCI_BATCH_SIZE': (232, 232), 
+                 'SCI_DROPOUT': (0.85, 0.85), 
+                 'SCI_L_SECOND': (132, 132), 
+                 'SCI_BN_MOMENTUM': (0.1, 0.1), 
+                 'SCI_SGD_MOMENTUM': (0, 0.999), 
+                 'SCI_BN_EPS':(12,12.99),
+                 'SCI_BN_STATS':(0,0.99),
+                 'SCI_LAST_LAYER':(1,1.99),
+                 'SCI_ACT_LAYER':(2,2.99)
+                },
+        verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+        random_state=1,
+    )
+        
+
+    #optimizer.maximize(
+        #n_iter=TRIALS, acq="ucb", kappa=0.1
+    #)
+    
+    optimizer.set_gp_params(normalize_y=True)
+    
+    optimizer.maximize(
+        init_points = RANDOM_STARTS,
+        n_iter = TRIALS,
+        #acq="ucb", kappa=0.1
+        #alpha=1e-3, n_restarts_optimizer=3
+        #acq="ei", xi=1e-4
+        kappa=5
+    )
+    
+    
+    print(optimizer.max)
+    
+    for i, res in enumerate(optimizer.res):
+        print("Iteration {}: \n\t{}".format(i, res))
+
+
+end.record()
+
+#print('Minimum Credit Cost: ',Min_Credit_Cost)
+
+print()
+print('Total execution time (minutes): ',start.elapsed_time(end)/60000)
+
+torch.cuda.empty_cache()
+
+if GET_STATS:
+    pr.disable()
+    s = io.StringIO()
+    sortby = SortKey.CUMULATIVE
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
+
+
+# In[ ]:
+
+
+
+
